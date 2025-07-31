7474686e22c184813754273f13e5c6fb3ff4ad33
irida22 <ilala22@epoka.edu.al>
2025-07-31 22:45:58 +0200

    frontend config  for deploy

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://localhost:5172',
+    BASE_URL: import.meta.env.VITE_API_URL || 'http://localhost:5172',
     ENDPOINTS: {
         LOGIN: '/api/login',
         BALANCE: '/api/users',
